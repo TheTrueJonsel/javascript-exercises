027fcb27c1a109982303d277b9ad75ce30175076
JonselLaptop <jonasmazou@gmail.com>
2024-09-16 19:29:43 +0200

    Finished exercises one to seven

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let result = []
+    outer: for(let element of array){
+        for(let currentElement of remove){
+            if(element === currentElement){
+                continue outer;
+            } 
+        }
+        result.push(element);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
+    if((typeof lowerBound) !== "number" || (typeof upperBound) !== "number"){
+        return "ERROR";
+    }
 
+    if(!Number.isInteger(lowerBound) || !Number.isInteger(upperBound)){
+        return "ERROR";
+    }
+
+    if(lowerBound < 0 || upperBound < 0){
+        return "ERROR";
+    }
+
+    if(lowerBound > upperBound){
+        let temp = lowerBound;
+        lowerBound = upperBound;
+        upperBound = temp;
+    }
+
+    return upperBound * (upperBound +1) / 2 - lowerBound * (lowerBound +1) / 2 + lowerBound;
 };
 
 // Do not edit below this line
